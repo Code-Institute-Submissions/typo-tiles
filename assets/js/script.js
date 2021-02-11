2cbc78ee12e291aa151c6cd7af7c881f9f32c1ca
RoxJade <roxannejade88@icloud.com>
2021-02-11 19:12:34 +0000

    solve 2 bug issues in the flip functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,27 @@
 const cards = document.querySelectorAll('.card');
 
 let hasFlipppedCard = false;
+let lockBoard = false;
 let firstCard, secondCard;
 
 // ------- Flipcard function...
 function flipCard() {
+    if (lockBoard) return;
+    if (this === firstCard) return;
+
     this.classList.add('flip');
 
     if (!hasFlipppedCard) {
         //first click
         hasFlipppedCard = true;
         firstCard = this;
-    } else {
-        //second click
-        hasFlipppedCard = false;
-        secondCard = this;
 
-        checkForMatch();
+        return;
     }
+    //second click
+    secondCard = this;
+
+    checkForMatch();
 }
 
 // ------- Check if cards match function...
@@ -35,14 +39,26 @@ function checkForMatch() {
 function disableCards() {
     firstCard.removeEventListener('click', flipCard);
     secondCard.removeEventListener('click', flipCard);
+
+    resetBoard();
 }
 
 // ------- Not a match function...
 function unFlipCards() {
+    lockBoard = true;
+
     setTimeout(() => {
         firstCard.classList.remove('flip');
         secondCard.classList.remove('flip');
+
+        resetBoard();
     }, 1500);
+}
+
+// ------- Reset board function...
+function resetBoard() {
+    [hasFlipppedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 }
 
 
