7bedf14074d2f08c0308b63199befc43086c5684
RoxJade <roxannejade88@icloud.com>
2021-02-14 01:55:24 +0000

    add timer and congratulations modal upon completion

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,14 +13,14 @@ function flipCard() {
     if (this === firstCard) return;
 
     this.classList.add('flip');
-    
-    
+
+
 
     if (!hasFlipppedCard) {
         //first click
         hasFlipppedCard = true;
         firstCard = this;
-        
+
 
         return;
     }
@@ -32,15 +32,23 @@ function flipCard() {
 
 // ------- Check if cards match function...
 function checkForMatch() {
+    let isMatch = firstCard.dataset.typeface === secondCard.dataset.typeface;
 
-    if (firstCard.dataset.typeface ===
-        secondCard.dataset.typeface) {
-        
+    if (isMatch) {
+        matchCounter += 1;
         disableCards();
+        if (matchCounter == (cards.length / 2)) {
+            congratulations();
+            //         window.alert("Congratulations! You Won!");
+        }
     } else {
         unFlipCards();
     }
 }
+//------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
+//------------------------------------------------------------------------------------------------
 
 // ------- It's a match function...
 function disableCards() {
@@ -78,7 +86,7 @@ function resetBoard() {
 
 cards.forEach(card => card.addEventListener('click', flipCard));
 
-function restartGame(){
+function restartGame() {
     location.reload();
 }
 
@@ -90,31 +98,31 @@ let counter = document.querySelector(".moves");
 // declare variables for star icons
 const stars = document.querySelectorAll(".fa-star");
 
- // stars list
- let starsList = document.querySelectorAll(".stars li");
+// stars list
+let starsList = document.querySelectorAll(".stars li");
 
 // @description count player's moves
-function moveCounter(){
+function moveCounter() {
     moves++;
     counter.innerHTML = moves;
-    /*//start timer on first click
-    if(moves == 1){
+    //start timer on first click
+    if (moves == 1) {
         second = 0;
-        minute = 0; 
+        minute = 0;
         hour = 0;
         startTimer();
-    }*/
+    }
     // setting rates based on moves
-    if (moves > 24 && moves < 32){
-        for( i= 0; i < 3; i++){
-            if(i > 1){
+    if (moves > 24 && moves < 32) {
+        for (i = 0; i < 3; i++) {
+            if (i > 1) {
                 stars[i].style.visibility = "collapse";
             }
         }
     }
-    else if (moves > 24){
-        for( i= 0; i < 3; i++){
-            if(i > 0){
+    else if (moves > 24) {
+        for (i = 0; i < 3; i++) {
+            if (i > 0) {
                 stars[i].style.visibility = "collapse";
             }
         }
@@ -122,8 +130,62 @@ function moveCounter(){
 }
 
 //----------------------------------------------------------------------------------------
+
+// @description game timer
+var second = 0, minute = 0; hour = 0;
+var timer = document.querySelector(".timer");
+var interval;
+function startTimer() {
+    interval = setInterval(function () {
+        timer.innerHTML = minute + "mins " + second + "secs";
+        second++;
+        if (second == 60) {
+            minute++;
+            second = 0;
+        }
+        if (minute == 60) {
+            hour++;
+            minute = 0;
+        }
+    }, 1000);
+}
+//Variable to count the card pairings
+let matchCounter = 0;
 //----------------------------------------------------------------------------------------
+
+ // close icon in modal
+ let closeicon = document.querySelector(".close");
+
+ // declare modal
+ let modal = document.getElementById("popup1")
+
+//let matchedCard = document.getElementsByClassName("match");
+// @description congratulations when all cards match, show modal and moves, time and rating
+function congratulations() {
+    //   if  (match.length == 16) {
+
+    clearInterval(interval);
+    finalTime = timer.innerHTML;
+
+    // show congratulations modal
+    modal.classList.add("show");
+
+    // declare star rating variable
+    var starRating = document.querySelector(".stars").innerHTML;
+
+    //showing move, rating, time on modal
+    document.getElementById("finalMove").innerHTML = moves;
+    document.getElementById("starRating").innerHTML = starRating;
+    document.getElementById("totalTime").innerHTML = finalTime;
+
+    //closeicon on modal
+    closeModal();
+};
+
+
+
 //---------------------------------------------------------------------------------------- 
+
 //----------------------------------------------------------------------------------------
 
 //  Memory game flip tile code adapted from: https://marina-ferreira.github.io/projects/js/memory-game/
